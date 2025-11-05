aabcc38d7e18e6a914904f6caa14bc8a0eff87fc
limavfabio <limavfabio@gmail.com>
2025-11-05 08:36:39 -0300

    wip wrangler install

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,17 @@ import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import svgr from "vite-plugin-svgr";
+import { cloudflare } from "@cloudflare/vite-plugin";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-	plugins: [tailwindcss(), reactRouter(), tsconfigPaths(), svgr()],
+	plugins: [
+		tailwindcss(),
+		reactRouter(),
+		tsconfigPaths(),
+		svgr(),
+		cloudflare({ viteEnvironment: { name: "ssr" } }),
+	],
 	resolve: {
 		alias: {
 			"@": path.resolve(__dirname, "./app"),
